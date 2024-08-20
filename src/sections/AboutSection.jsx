8385373487e29c83780f5dfbe564ce7f5abaf19b
GitHubNexon <johnmarkpulmano052802@gmail.com>
2024-08-20 18:21:10 +0800

    fix some bugs and overflowing in aboutpage

--- src/sections/AboutSection.jsx
+++ src/sections/AboutSection.jsx
@@ -4,7 +4,7 @@ import aboutData from "../data/aboutData";
 
 const AboutSection = () => {
   return (
-    <div className="flex items-center flex-col">
+    <div className="flex items-center flex-col overflow-x-hidden">
       <h2 className="text-gray-900 text-center py-5 font-bold text-5xl">
         ABOUT US
       </h2>
